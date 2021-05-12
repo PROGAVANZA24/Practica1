5bf40ee794d1b56269b380a5ac8a81883a90de69
CarlosMarrufo <carlos-al.ma@hotmail.com>
2021-05-12 01:03:51 -0500

    Creación del módulo guardarNumeros y modificación bloque principal (29)

--- CarlosMarrufo.py
+++ CarlosMarrufo.py
@@ -1,3 +1,18 @@
+#Guardar numeros
+def guardarNumeros():
+    numerosGuardar = int(input("¿Cuántos números vas a registrar?: "))
+    listaNumeros = []
+    for numero in range(numerosGuardar):
+        numeroGuardar = int(input(f'Ingrese el número a guardar (restan {numerosGuardar - numero}): '))
+        listaNumeros.append(numeroGuardar)
+    f = open("numeros.txt", "w")
+    for numero in listaNumeros:
+        f.write(f'{numero}\n')
+    f.close
+    print("Numeros registrados en el archivo.")
+    print("Saliendo del sistema...")
+    exit()
+
 #Bloque principal
 seleccionUsuario = 4
 print("*"*60)
@@ -11,7 +26,7 @@ while seleccionUsuario < 1 or seleccionUsuario > 3:
     print("*"*60)
     seleccionUsuario = int(input("Ingrese una opción: "))
     if seleccionUsuario == 1:
-        pass
+        guardarNumeros()
     elif seleccionUsuario == 2:
         pass
     elif seleccionUsuario == 3:
