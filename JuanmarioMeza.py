d303ddde7d6a3d1769834802f5fd722444293ce1
Prog-Josafat <juanmario.mezard@uanl.edu.mx>
2021-05-13 18:45:48 -0500

    se agrego la calse JuanmarioMeza.py

--- JuanmarioMeza.py
+++ JuanmarioMeza.py
@@ -4,19 +4,4 @@ class listadenumeros:
         self.montos=montos
         self.contador=contador
 
-    def listanumeros(variable, montos, contador):
-        variable = int(input(f'Introduce la cantidad de números a capturar{variable+1}: '))
-        montos = list()
-
-        contador=0
-        while contador < variable:
-            montos.append(input('Captura el número: '))
-            contador = contador + 1
-            print(montos)
-
-    def buscarunnumero(variable, montos, contador):
-        montos.index(int('Escriba el numero que desea buscar: '))
-        if (montos.index)== True:
-            print('Su número ha sido encontrado')
-        else:
-            print('Su número no ha sido encontrado')
+    
