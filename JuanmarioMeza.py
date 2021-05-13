280cb5b44013b38efe88e8e939511ccecbbbd6b8
Prog-Josafat <juanmario.mezard@uanl.edu.mx>
2021-05-13 18:46:40 -0500

    Se agrego el primer metodo de la lista JuanmarioMeza.py

--- JuanmarioMeza.py
+++ JuanmarioMeza.py
@@ -3,5 +3,16 @@ class listadenumeros:
         self.variable=variable
         self.montos=montos
         self.contador=contador
+        
+def listanumeros(variable, montos, contador):
+        variable = int(input(f'Introduce la cantidad de números a capturar{variable+1}: '))
+        montos = list()
 
+        contador=0
+        while contador < variable:
+            montos.append(input('Captura el número: '))
+            contador = contador + 1
+            print(montos)
+
+   
     
