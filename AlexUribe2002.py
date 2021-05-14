93e189be1853fef5f35261dccc9090746915e37d
AlexUribe2002 <78452393+AlexUribe2002@users.noreply.github.com>
2021-05-13 19:09:00 -0500

    Create AlexUribe2002.py

---
+++ AlexUribe2002.py
@@ -0,0 +1,11 @@
+archivo=open('Numeros_lista.txt','r')
+'''archivo=str()'''
+'''print (archivo.read())'''
+num=input('Numero que desea encontrar: ')
+for linea in archivo:
+    if num in linea:
+        print('Numero encontrado')
+    else:
+        print('No encontrado')
+        
+    
